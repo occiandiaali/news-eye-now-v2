758fa1db7a5ac1b3655ad643a4f8e9258c90c392
ocean <ocean@localhost.localdomain>
2019-07-12 20:30:08 +0100

    add url constants, get page function and new category route

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -5,11 +5,20 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ApiService {
-  API_TOP_URL = 'https://newsapi.org/v2/top-headlines?country=ng&apiKey=40f233687a574369b42c8cd177ab60e0';
+  API_KEY = '&apiKey=40f233687a574369b42c8cd177ab60e0';
+  category = '';
+  API_TOP_URL = `https://newsapi.org/v2/top-headlines?country=ng${this.API_KEY}`;
+  urlPre = `https://newsapi.org/v2/top-headlines?country=ng&category=`;
 
   constructor(private http: HttpClient) { }
 
   getHeadlines() {
     return this.http.get(`${this.API_TOP_URL}`);
   }
+
+  getPage(category) {
+    this.category = category;
+    return this.http.get(`${this.urlPre}${this.category}${this.API_KEY}`);
+  }
+
 } // class
