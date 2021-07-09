e23c9a40524309336e37e9b9b34089ef5cb466ab
Occian Fumnanya Diaali <ocean.diaali@gmail.com>
2021-07-09 16:27:51 +0100

    Fix typo in test case

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,7 +7,7 @@ describe('new App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display a welcome message', () => {
     page.navigateTo();
     expect(page.getPageTitle()).toContain('Tab One');
   });
